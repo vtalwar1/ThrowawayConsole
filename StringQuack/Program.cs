b2cef86cb37fce9c64a6fe0eb021046709244091
Vidit Talwar <vtalwar1996@gmail.com>
2016-09-12 16:56:01 -0600

    Simple Console Application

---
+++ StringQuack/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringQuack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String message = "Hello World";
+            Console.WriteLine(message.Quack());
+        }
+    }
+    public static class StringExtensions
+    {
+        public static String Quack(this string self)
+        {
+            return self + " (quack)";
+        }
+    }
+}
